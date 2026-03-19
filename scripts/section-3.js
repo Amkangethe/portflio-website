d063121e744709c594fd55165b0d57daa2d776b6
Allan Kangethe <amkangethe@outlook.com>
2026-03-19 00:32:33 -0400

    finished projects section

--- scripts/section-3.js
+++ scripts/section-3.js
@@ -7,9 +7,9 @@
 
 const timelineItems = [
   { order: 20, side: "left", date: "June 2024 - Dec 2024", title: "WezeshaNet Technologies Ltd", subtitle: "Software Engineer Intern", icon: "./icons/experience/wezeshanet.png", link: "https://www.wezeshanet.com", tags: ["Utility Scripts", "Debugging", "Testing", "Agile Sprints", "Git"] },
-  { order: 10, side: "right", date: "Jan 2026 - Present", title: "Master's of Science", subtitle: "Georgia Institute of Technology", icon: "./icons/schools/gatech.png", link: "https://www.gatech.edu", tags: ["Machine Learning", "Software Engineering", "Cloud"] },
-  { order: 30, side: "right", date: "Sept 2022 - May 2025", title: "Bachelor's of Science", subtitle: "University of Maryland, Baltimore County", icon: "./icons/schools/umbc.png", link: "https://umbc.edu", tags: ["AI", "Data Structures", "Algorithms", "Software Engineering", "Databases", "Probability", "Statistics"] },
-  { order: 40, side: "right", date: "Sept 2020 - May 2022", title: "Associate's of Science", subtitle: "Montgomery College", icon: "./icons/schools/mc.png", link: "https://www.montgomerycollege.edu", tags: ["Programming", "Software Design", "Algorithms"] },
+  { order: 10, side: "right", date: "Jan 2026 - Present", title: "Master of Science", subtitle: "Georgia Institute of Technology", icon: "./icons/schools/gatech.png", link: "https://www.gatech.edu", tags: ["Machine Learning", "Software Engineering", "Cloud"] },
+  { order: 30, side: "right", date: "Sept 2022 - May 2025", title: "Bachelor of Science", subtitle: "University of Maryland, Baltimore County", icon: "./icons/schools/umbc.png", link: "https://umbc.edu", tags: ["AI", "Data Structures", "Algorithms", "Software Engineering", "Databases", "Probability", "Statistics"] },
+  { order: 40, side: "right", date: "Sept 2020 - May 2022", title: "Associate of Science", subtitle: "Montgomery College", icon: "./icons/schools/mc.png", link: "https://www.montgomerycollege.edu", tags: ["Programming", "Software Design", "Algorithms"] },
 ];
 
 function sortTimeline(items) {
---
+++ scripts/section-4.js
@@ -0,0 +1,79 @@
+// --------------------------------------------------------------------------------------
+// Projects Section
+projects = [{
+        thumbnail : 'icons/projects/breast-cancer.png',
+        title: 'Breast Cancer Classifier', 
+        description: 'Built a deep learning image-classification model to identify' +
+        'breast tissue images as cancerous or non-cancerous. Used TensorFlow/Keras' +
+        'with a pretrained ResNet50 backbone, prepared training/validation/test datasets,' +
+        'and evaluated model performance for medical image classification.',
+        technologies : ['Python', 'TensorFlow', 'Keras', 'ResNet50', 
+            'NumPy', 'Matplotlib', 'Pandas', 'Scikit-learn','Jupyter Notebook'],
+        link : 'https://github.com/Nalan12/AI4ALL_Project'
+    }
+];
+let projectsHTML = '';
+
+projects.forEach((project) => {
+    
+    project.technologies.forEach((technology) => {
+        console.log(technology);
+    });
+    projectsHTML += 
+    `
+    <div class="project-card">
+        <div class="project-card-thumnail">
+            <img class="project-card-thumnail-img" src="${project.thumbnail}" alt="" />
+        </div>
+        <div class="project-card-info">
+            <p class="project-title">${project.title}</p>
+            <p class="project-description">${project.description}</p>
+            <p class="project-technologies-text">Technologies:</p>
+            <div class="project-technologies">
+            `;
+            project.technologies.forEach((technology) => {
+                projectsHTML += `<div class="project-technologies-chip">${technology}</div>`;
+            });
+    projectsHTML += `
+            </div>
+            <a class="project-code-button" href="${project.link}" target="_blank"  rel="noopener noreferrer">
+                <img class="project-code-img" src="icons/skills/github-code.png" alt="" />
+                <span class="project-code-text">Code</span>
+            </a>
+        </div>
+    </div>	
+    `;
+    
+
+});
+
+document.querySelector(".js-projects-container").innerHTML = projectsHTML;
+
+/*  
+skillsSection = [{ "Languages" : ['Python', 'Java', 'C++', 'JavaScript', 'SQL', 'C#']},
+                { "Web Development" : ['HTML', 'CSS', 'React', 'Flask']},
+                { "AI & ML" : ['Ollama', 'TensorFlow', 'Keras', 'Scikit-Learn', 'Pandas', 'NumPy', 'Matplotlib']},
+                { "Databases & Tools" : ['MySQL','SQLite', 'MongoDB', 'Git', 'Linux', 'Jira']}
+];
+
+let skillsSectionHTML = '';
+
+skillsSection.forEach((group) => {
+  const category = Object.keys(group)[0];
+  const skills = group[category];
+
+  const chipsHTML = skills
+    .map((skill) => `<span class="chip">${skill}</span>`)
+    .join("");
+
+  skillsSectionHTML += `
+    <div class="tech-card">
+      <p class="tech-card-title">${category}</p>
+      <div class="tech-card-chips">
+        ${chipsHTML}
+      </div>
+    </div>
+  `;
+});
+
+document.querySelector(".js-technical-grid-container").innerHTML = skillsSectionHTML;*/
